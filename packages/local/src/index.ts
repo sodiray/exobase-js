b008b8807c94ae3a8a6b54131a85b09e8541cb05
Ray Epps <rayharryepps@gmail.com>
2021-12-12 13:13:07 -0700

    remove binary from local

--- packages/local/src/index.ts
+++ packages/local/src/index.ts
@@ -1,54 +1,32 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+import express from 'express'
+import type { Request, Response } from 'express'
+import bodyParser from 'body-parser'
 import _ from 'radash'
-import express, { Request, Response } from 'express'
-import { json } from 'body-parser'
+import path from 'path'
 import fs from 'fs'
 import type { FrameworkMapper } from './frameworks/types'
-import expresFrameworkMapper from './frameworks/express'
+import expressFrameworkMapper from './frameworks/express'
 
-type Args = {
-  port?: string
-  framework?: FrameworkMapper
-  json?: boolean
-}
+export type { FrameworkMapper } from './frameworks/types'
+export { default as expressFrameworkMapper } from './frameworks/express'
+export { default as lambdaFrameworkMapper } from './frameworks/lambda'
+export { default as vercelFrameworkMapper } from './frameworks/vercel'
+
 
 type ModuleFunction = {
   function: string
   module: string
+}
+
+type ModuleFunctionLocation = ModuleFunction & {
   paths: {
     import: string
     file: string
   }
 }
 
-export default async function run({
-  port = '8000',
-  framework = expresFrameworkMapper,
-  json: useJson = true
-}: Args, cb?: (port: number) => void) {
-
-  const api = express()
-  if (useJson) api.use(json())
-
-  const localized = (func: Function) => async (req: Request, res: Response) => {
-    const args = await framework.mapRequestToArgs(req, res)
-    const result = await func(...args)
-    framework.mapResultToRes(req, res, result)
-  }
-
-  // Add each endpoint to the local running 
-  // express app
-  for (const f of getFunctionMap()) {
-    const { default: func } = await import(f.paths.import)
-    api.all(`/${f.module}/${f.function}`, localized(func))
-  }
-
-  // Get it poppin bebe
-  const p = parseInt(port)
-  api.listen(p, () => {
-    cb?.(p)
-  })
-
+type ModuleFunctionSource = ModuleFunction & {
+  func: Function
 }
 
 
@@ -57,9 +35,8 @@ export default async function run({
  * functions. Returns their names and locations
  * as an array.
  */
-function getFunctionMap(): ModuleFunction[] {
-  const cwd = process.cwd()
-  const relPath = (rel: string) => `${cwd}${rel}`
+export function getFunctionMap(rootPath: string): ModuleFunctionLocation[] {
+  const relPath = (rel: string) => path.resolve(rootPath, rel)
   const modules = fs.readdirSync(relPath('/src/modules'), { withFileTypes: true })
     .filter(item => item.isDirectory())
     .map(m => {
@@ -76,7 +53,42 @@ function getFunctionMap(): ModuleFunction[] {
               import: relPath(`/src/modules/${m.name}/${funcName}`)
             }
           }
-        }) as ModuleFunction[]
+        }) as ModuleFunctionLocation[]
     })
   return _.flat(modules)
+}
+
+export async function start({
+  port = '8000',
+  framework = expressFrameworkMapper,
+  json: useJson = true,
+  functions = []
+}: {
+  port?: string
+  framework?: FrameworkMapper
+  json?: boolean
+  functions: ModuleFunctionSource[]
+}, cb?: (port: number) => void) {
+
+  const api = express()
+  if (useJson) api.use(bodyParser.json())
+
+  const mapped = (func: Function) => async (req: Request, res: Response) => {
+    const args = await framework.mapRequestToArgs(req, res)
+    const result = await func(...args)
+    framework.mapResultToRes(req, res, result)
+  }
+
+  // Add each endpoint to the local running 
+  // express app
+  for (const f of functions) {
+    api.all(`/${f.module}/${f.function}`, mapped(f.func))
+  }
+
+  // Get it poppin bebe
+  const p = parseInt(port)
+  api.listen(p, () => {
+    cb?.(p)
+  })
+
 }
