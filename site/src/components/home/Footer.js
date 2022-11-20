398eef09eae93d3ea4aa9c6652fa21f83878e261
Ray Epps <rayepps@vessel.local>
2022-11-20 13:01:51 -0700

    fix footer styles + add all docs pages to footer

--- site/src/components/home/Footer.js
+++ site/src/components/home/Footer.js
@@ -1,17 +1,14 @@
 import Link from 'next/link'
 import { documentationNav } from '@/navs/documentation'
 import { Logo } from '@/components/Logo'
+import { listify, mapEntries } from 'radash'
 
-const footerNav = [
-  {
-    'Getting Started': documentationNav['Getting Started']
-  }
-]
+const footerNav = listify(documentationNav, (key, value) => ({ [key]: value }))
 
 export function Footer() {
   return (
-    <footer className="pb-16 text-sm leading-6">
-      <div className="max-w-7xl mx-auto divide-y divide-slate-200 px-4 sm:px-6 md:px-8 dark:divide-slate-700">
+    <footer className="pb-12 text-sm leading-6">
+      <div className="max-w-7xl mx-auto divide-y divide-slate-200 px-4 sm:px-6 md:px-8">
         <div className="flex">
           {footerNav.map((sections) => (
             <div
@@ -20,12 +17,12 @@ export function Footer() {
             >
               {Object.entries(sections).map(([title, items]) => (
                 <div key={title} className="lg:flex-none lg:w-1/2">
-                  <h2 className="font-semibold text-slate-900 dark:text-slate-100">{title}</h2>
+                  <h2 className="font-semibold text-slate-900">{title}</h2>
                   <ul className="mt-3 space-y-2">
                     {items.map((item) => (
                       <li key={item.href}>
                         <Link href={item.href}>
-                          <a className="hover:text-slate-900 dark:hover:text-slate-300">
+                          <a className="hover:text-slate-900">
                             {item.title}
                           </a>
                         </Link>
